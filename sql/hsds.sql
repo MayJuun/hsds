4d37d41c04dbb571a4a4ddd1bd442c183ecec0b0
Dokotela <grey.faulkenberry@mayjuun.com>
2022-04-23 21:59:48 +0200

    Updating sql script

--- sql/hsds.sql
+++ sql/hsds.sql
@@ -1,221 +1,330 @@
-    CREATE TABLE organization (
-        id UUID UNIQUE NOT NULL KEY,
-        name VARCHAR (255) NOT NULL,
-        alternate_name VARCHAR (255),
-        description VARCHAR (255) NOT NULL,
-        email VARCHAR (255),
-        url VARCHAR (255),
-        tax_status VARCHAR (255),
-        tax_id VARCHAR (255),
-        year_incorporated DATE (255),
-        legal_status VARCHAR (255),
-    );
+-- organization
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "organization";
 
-    CREATE TABLE program (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (organization_id) REFERENCES organization (id) UUID UNIQUE NOT NULL,
-        name VARCHAR (255) NOT NULL,
-        alternate_name VARCHAR (255),
-    );
+CREATE TABLE "organization" (
+    "id" UUID NOT NULL UNIQUE,
+    "name" VARCHAR (255) NOT NULL,
+    "alternate_name" VARCHAR (255),
+    "description" VARCHAR (255) NOT NULL,
+    "email" VARCHAR (255),
+    "url" VARCHAR (255),
+    "tax_status" VARCHAR (255),
+    "tax_id" VARCHAR (255),
+    "year_incorporated" DATE,
+    "legal_status" VARCHAR (255),
+    Primary KEY ("id")
+);
 
-    CREATE TABLE service (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (organization_id) REFERENCES organization (id) UUID NOT NULL,
-        FOREIGN KEY (program_id) REFERENCES program (id) VARCHAR (255) NOT NULL,
-        name VARCHAR (255) NOT NULL,
-        alternate_name VARCHAR (255),
-        description VARCHAR (255),
-        url VARCHAR (255),
-        email VARCHAR (255),
-        status VARCHAR (255) NOT NULL,
-        interpretation_services VARCHAR (255),
-        application_process VARCHAR (255),
-        wait_time VARCHAR (255),
-        fees VARCHAR (255),
-        accreditations VARCHAR (255),
-        licenses VARCHAR (255),
-    );
+-- program
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "program";
 
-    CREATE TABLE service_attribute (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255) NOT NULL,
-        FOREIGN KEY (taxonomy_term_id) REFERENCES taxonomy_term (id) VARCHAR (255),
-    );
+CREATE TABLE "program" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "organization_id" UUID REFERENCES "organization" ("id") NOT NULL UNIQUE,
+    "name" VARCHAR (255) NOT NULL,
+    "alternate_name" VARCHAR (255),
+    Primary KEY ("id")
+);
 
-    CREATE TABLE other_attribute (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        link_id VARCHAR (255) NOT NULL,
-        link_type VARCHAR (255) NOT NULL,
-        FOREIGN KEY (taxonomy_term_id) REFERENCES taxonomy_term (id) VARCHAR (255),
-    );
+-- service
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "service";
 
-    CREATE TABLE service_at_location (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255) NOT NULL,
-        FOREIGN KEY (location_id) REFERENCES location (id) VARCHAR (255),
-        description VARCHAR (255),
-    );
+CREATE TABLE "service" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "organization_id" UUID REFERENCES "organization" ("id") NOT NULL,
+    "program_id" VARCHAR(255) REFERENCES "program" ("id"),
+    "name" VARCHAR (255) NOT NULL,
+    "alternate_name" VARCHAR (255),
+    "description" VARCHAR (255),
+    "url" VARCHAR (255),
+    "email" VARCHAR (255),
+    "status" VARCHAR (255) NOT NULL,
+    "interpretation_services" VARCHAR (255),
+    "application_process" VARCHAR (255),
+    "wait_time" VARCHAR (255),
+    "fees" VARCHAR (255),
+    "accreditations" VARCHAR (255),
+    "licenses" VARCHAR (255),
+    Primary KEY ("id")
+);
 
-    CREATE TABLE location (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        organization_id VARCHAR (255),
-        name VARCHAR (255),
-        alternate_name VARCHAR (255),
-        description VARCHAR (255),
-        transportation VARCHAR (255),
-        latitude FLOAT,
-        longitude FLOAT,
-    );
+-- taxonomy_term
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "taxonomy_term";
 
-    CREATE TABLE phone (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (location_id) REFERENCES location (id) VARCHAR (255),
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255),
-        FOREIGN KEY (organization_id) REFERENCES organization (id) UUID,
-        FOREIGN KEY (contact_id) REFERENCES contact (id) VARCHAR (255),
-        FOREIGN KEY (taxonomy_term_id) REFERENCES taxonomy_term (id) VARCHAR (255),
-        FOREIGN KEY (service_at_location_id) REFERENCES service_at_location (id) VARCHAR (255),
-        number VARCHAR (255) NOT NULL,
-        extension FLOAT,
-        type VARCHAR (255),
-        language VARCHAR (255),
-        description VARCHAR (255),
-        department VARCHAR (255),
-    );
+CREATE TABLE "taxonomy_term" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "term" VARCHAR (255) NOT NULL,
+    "description" VARCHAR (255) NOT NULL,
+    "parent_id" VARCHAR (255),
+    "taxonomy" VARCHAR (255),
+    "language" VARCHAR (255),
+    Primary KEY ("id")
+);
 
-    CREATE TABLE contact (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (organization_id) REFERENCES organization (id) UUID,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255),
-        FOREIGN KEY (service_at_location_id) REFERENCES service_at_location (id) VARCHAR (255),
-        name VARCHAR (255),
-        title VARCHAR (255),
-        department VARCHAR (255),
-        email VARCHAR (255),
-    );
+-- service_attribute
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "service_attribute";
 
-    CREATE TABLE physical_address (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (location_id) REFERENCES location (id) VARCHAR (255),
-        attention VARCHAR (255),
-        address_1 VARCHAR (255) NOT NULL,
-        address_2 VARCHAR (255),
-        address_3 VARCHAR (255),
-        address_4 VARCHAR (255),
-        city VARCHAR (255) NOT NULL,
-        region VARCHAR (255),
-        state_province VARCHAR (255) NOT NULL,
-        postal_code VARCHAR (255) NOT NULL,
-        country VARCHAR (255) NOT NULL,
-    );
+CREATE TABLE "service_attribute" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "service_id" VARCHAR(255) REFERENCES "service" ("id") NOT NULL,
+    "taxonomy_term_id" VARCHAR(255) REFERENCES "taxonomy_term" ("id"),
+    Primary KEY ("id")
+);
 
-    CREATE TABLE postal_address (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (location_id) REFERENCES location (id) VARCHAR (255),
-        attention VARCHAR (255),
-        address_1 VARCHAR (255) NOT NULL,
-        address_2 VARCHAR (255),
-        address_3 VARCHAR (255),
-        address_4 VARCHAR (255),
-        city VARCHAR (255) NOT NULL,
-        region VARCHAR (255),
-        state_province VARCHAR (255) NOT NULL,
-        postal_code VARCHAR (255) NOT NULL,
-        country VARCHAR (255) NOT NULL,
-    );
+-- other_attribute
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "other_attribute";
 
-    CREATE TABLE schedule (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255),
-        FOREIGN KEY (location_id) REFERENCES location (id) VARCHAR (255),
-        FOREIGN KEY (service_at_location_id) REFERENCES service_at_location (id) VARCHAR (255),
-        valid_from DATE,
-        valid_to DATE,
-        dtstart DATE,
-        timezone FLOAT,
-        until DATE,
-        count DATE,
-        wkst DATE,
-        freq VARCHAR (255),
-        interval FLOAT,
-        byday VARCHAR (255),
-        byweekno VARCHAR (255),
-        bymonthday FLOAT,
-        byyearday FLOAT,
-        description VARCHAR (255),
-        opens_at TIME,
-        closes_at TIME,
-    );
+CREATE TABLE "other_attribute" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "link_id" VARCHAR (255) NOT NULL,
+    "link_type" VARCHAR (255) NOT NULL,
+    "taxonomy_term_id" VARCHAR(255) REFERENCES "taxonomy_term" ("id"),
+    Primary KEY ("id")
+);
 
-    CREATE TABLE funding (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (organization_id) REFERENCES organization (id) UUID,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255),
-        source VARCHAR (255),
-    );
+-- location
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "location";
 
-    CREATE TABLE eligibility (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255),
-    );
+CREATE TABLE "location" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "organization_id" UUID REFERENCES "organization" ("id"),
+    "name" VARCHAR (255),
+    "alternate_name" VARCHAR (255),
+    "description" VARCHAR (255),
+    "transportation" VARCHAR (255),
+    "latitude" FLOAT,
+    "longitude" FLOAT,
+    Primary KEY ("id")
+);
 
-    CREATE TABLE service_area (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255),
-        service_area VARCHAR (255),
-        description VARCHAR (255),
-    );
+-- service_at_location
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "service_at_location";
 
-    CREATE TABLE required_document (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255),
-        document VARCHAR (255),
-    );
+CREATE TABLE "service_at_location" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "service_id" VARCHAR(255) REFERENCES "service" ("id") NOT NULL,
+    "location_id" VARCHAR(255) REFERENCES "location" ("id") NOT NULL,
+    "description" VARCHAR (255),
+    Primary KEY ("id")
+);
 
-    CREATE TABLE payment_accepted (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255),
-        payment VARCHAR (255),
-    );
+-- contact
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "contact";
 
-    CREATE TABLE language (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (service_id) REFERENCES service (id) VARCHAR (255),
-        FOREIGN KEY (location_id) REFERENCES location (id) VARCHAR (255),
-        language VARCHAR (255),
-    );
+CREATE TABLE "contact" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "organization_id" UUID REFERENCES "organization" ("id"),
+    "service_id" VARCHAR(255) REFERENCES "service" ("id"),
+    "service_at_location_id" VARCHAR(255) REFERENCES "service_at_location" ("id"),
+    "name" VARCHAR (255),
+    "title" VARCHAR (255),
+    "department" VARCHAR (255),
+    "email" VARCHAR (255),
+    Primary KEY ("id")
+);
 
-    CREATE TABLE accessibility_for_disabilities (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        FOREIGN KEY (location_id) REFERENCES location (id) VARCHAR (255),
-        accessibility VARCHAR (255),
-        details VARCHAR (255),
-    );
+-- phone
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "phone";
 
-    CREATE TABLE taxonomy_term (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        term VARCHAR (255) NOT NULL,
-        description VARCHAR (255) NOT NULL,
-        parent_id VARCHAR (255),
-        taxonomy VARCHAR (255),
-        language VARCHAR (255),
-    );
+CREATE TABLE "phone" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "location_id" VARCHAR(255) REFERENCES "location" ("id"),
+    "service_id" VARCHAR(255) REFERENCES "service" ("id"),
+    "organization_id" UUID REFERENCES "organization" ("id"),
+    "contact_id" VARCHAR(255) REFERENCES "contact" ("id"),
+    "service_at_location_id" VARCHAR(255) REFERENCES "service_at_location" ("id"),
+    "number" VARCHAR (255) NOT NULL,
+    "extension" FLOAT,
+    "type" VARCHAR (255),
+    "language" VARCHAR (255),
+    "description" VARCHAR (255),
+    "department" VARCHAR (255),
+    Primary KEY ("id")
+);
 
-    CREATE TABLE metadata (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        resource_id VARCHAR (255) UNIQUE NOT NULL,
-        resource_type VARCHAR (255) UNIQUE NOT NULL,
-        last_action_date VARCHAR (255) NOT NULL,
-        last_action_type DATETIME NOT NULL,
-        field_name VARCHAR (255) NOT NULL,
-        previous_value VARCHAR (255) NOT NULL,
-        replacement_value VARCHAR (255) NOT NULL,
-        updated_by VARCHAR (255) NOT NULL,
-    );
+-- physical_address
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "physical_address";
 
-    CREATE TABLE meta_table_description (
-        id VARCHAR (255) UNIQUE NOT NULL KEY,
-        name VARCHAR (255),
-        language VARCHAR (255),
-        character_set VARCHAR (255),
-    );
+CREATE TABLE "physical_address" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "location_id" VARCHAR(255) REFERENCES "location" ("id"),
+    "attention" VARCHAR (255),
+    "address_1" VARCHAR (255) NOT NULL,
+    "address_2" VARCHAR (255),
+    "address_3" VARCHAR (255),
+    "address_4" VARCHAR (255),
+    "city" VARCHAR (255) NOT NULL,
+    "region" VARCHAR (255),
+    "state_province" VARCHAR (255) NOT NULL,
+    "postal_code" VARCHAR (255) NOT NULL,
+    "country" VARCHAR (255) NOT NULL,
+    Primary KEY ("id")
+);
+
+-- postal_address
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "postal_address";
+
+CREATE TABLE "postal_address" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "location_id" VARCHAR(255) REFERENCES "location" ("id"),
+    "attention" VARCHAR (255),
+    "address_1" VARCHAR (255) NOT NULL,
+    "address_2" VARCHAR (255),
+    "address_3" VARCHAR (255),
+    "address_4" VARCHAR (255),
+    "city" VARCHAR (255) NOT NULL,
+    "region" VARCHAR (255),
+    "state_province" VARCHAR (255) NOT NULL,
+    "postal_code" VARCHAR (255) NOT NULL,
+    "country" VARCHAR (255) NOT NULL,
+    Primary KEY ("id")
+);
+
+-- schedule
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "schedule";
+
+CREATE TABLE "schedule" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "service_id" VARCHAR(255) REFERENCES "service" ("id"),
+    "location_id" VARCHAR(255) REFERENCES "location" ("id"),
+    "service_at_location_id" VARCHAR(255) REFERENCES "service_at_location" ("id"),
+    "valid_from" DATE,
+    "valid_to" DATE,
+    "dtstart" DATE,
+    "timezone" SMALLINT,
+    "until" DATE,
+    "count" DATE,
+    "wkst" DATE,
+    "freq" VARCHAR (255),
+    "interval" SMALLINT,
+    "byday" VARCHAR (255),
+    "byweekno" VARCHAR (255),
+    "bymonthday" SMALLINT,
+    "byyearday" SMALLINT,
+    "description" VARCHAR (255),
+    "opens_at" TIME,
+    "closes_at" TIME,
+    Primary KEY ("id")
+);
+
+-- funding
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "funding";
+
+CREATE TABLE "funding" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "organization_id" UUID REFERENCES "organization" ("id"),
+    "service_id" VARCHAR(255) REFERENCES "service" ("id"),
+    "source" VARCHAR (255),
+    Primary KEY ("id")
+);
+
+-- eligibility
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "eligibility";
+
+CREATE TABLE "eligibility" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "service_id" VARCHAR(255) REFERENCES "service" ("id"),
+    Primary KEY ("id")
+);
+
+-- service_area
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "service_area";
+
+CREATE TABLE "service_area" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "service_id" VARCHAR(255) REFERENCES "service" ("id"),
+    "service_area" VARCHAR (255),
+    "description" VARCHAR (255),
+    Primary KEY ("id")
+);
+
+-- required_document
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "required_document";
+
+CREATE TABLE "required_document" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "service_id" VARCHAR(255) REFERENCES "service" ("id"),
+    "document" VARCHAR (255),
+    Primary KEY ("id")
+);
+
+-- payment_accepted
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "payment_accepted";
+
+CREATE TABLE "payment_accepted" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "service_id" VARCHAR(255) REFERENCES "service" ("id"),
+    "payment" VARCHAR (255),
+    Primary KEY ("id")
+);
+
+-- language
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "language";
+
+CREATE TABLE "language" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "service_id" VARCHAR(255) REFERENCES "service" ("id"),
+    "location_id" VARCHAR(255) REFERENCES "location" ("id"),
+    "language" VARCHAR (255),
+    Primary KEY ("id")
+);
+
+-- accessibility_for_disabilities
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "accessibility_for_disabilities";
+
+CREATE TABLE "accessibility_for_disabilities" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "location_id" VARCHAR(255) REFERENCES "location" ("id"),
+    "accessibility" varchar(50) DEFAULT NULL,
+    "details" text,
+    Primary KEY ("id")
+);
+
+-- metadata
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "metadata";
+
+CREATE TABLE "metadata" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "resource_id" VARCHAR (255) NOT NULL UNIQUE,
+    "resource_type" VARCHAR (255) NOT NULL UNIQUE,
+    "last_action_date" VARCHAR (255) NOT NULL,
+    "last_action_type" TIMESTAMP NOT NULL,
+    "field_name" VARCHAR (255) NOT NULL,
+    "previous_value" VARCHAR (255) NOT NULL,
+    "replacement_value" VARCHAR (255) NOT NULL,
+    "updated_by" VARCHAR (255) NOT NULL,
+    Primary KEY ("id")
+);
+
+-- meta_table_description
+--------------------------------------------------------------
+DROP TABLE IF EXISTS "meta_table_description";
+
+CREATE TABLE "meta_table_description" (
+    "id" VARCHAR (255) NOT NULL UNIQUE,
+    "name" VARCHAR (255),
+    "language" VARCHAR (255),
+    "character_set" VARCHAR (255),
+    Primary KEY ("id")
+);
